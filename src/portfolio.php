87fa9cbb254983c655e3c7cc4e989857c75452cf
Joris Berthelot <admin@eexit.net>
2011-08-11 11:15:11 +0200

    Added Symfony Validator component + extension activation

--- src/portfolio.php
+++ src/portfolio.php
@@ -1,17 +1,25 @@
 <?php
+use Symfony\Component\Validator\Constraints;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+// Bootstraping
 require_once __DIR__ . '/../vendor/Silex/silex.phar';
-
 $app = new Silex\Application();
 
+// Registers Symfony Validator component extension
+$app->register(new Silex\Extension\ValidatorExtension(), array(
+   'validator.class_path'   => __DIR__ . '/../vendor/Symfony/src' 
+));
+
+// Registers Twig extension
 $app->register(new Silex\Extension\TwigExtension(), array(
    'twig.class_path'    => __DIR__ . '/../vendor/Twig/lib',
    'twig.path'          => __DIR__ . '/views'
 ));
 
+// Registers Monolog extension
 $app->register(new Silex\Extension\MonologExtension(), array(
     'monolog.class_path'    => __DIR__ . '/../vendor/monolog/src',
     'monolog.logfile'       => __DIR__ . '/../log/portfolio.log',
@@ -19,6 +27,7 @@ $app->register(new Silex\Extension\MonologExtension(), array(
     'monolog.level'         => 300
 ));
 
+// Application error handling
 $app->error(function(\Exception $e) use ($app) {
     if ($e instanceof NotFoundHttpException) {
         $content = vsprintf('<h1>%d - %s (%s)</h1>', array(
@@ -33,6 +42,10 @@ $app->error(function(\Exception $e) use ($app) {
         return new Response('<p>You should go eat some cookies while we\'re fixing this feature!', $e->getStatusCode());
     }
 });
+
+// ======================
+// = Route declarations =
+// ======================
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('index.twig');
